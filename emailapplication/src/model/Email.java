a501f4afa135a678ffb22119a5f289c44afaed8c
Carla Mariana <carlamab2808@outlook.com>
2024-02-28 10:43:53 -0300

    defining the email department

--- emailapplication/src/model/Email.java
+++ emailapplication/src/model/Email.java
@@ -1,5 +1,7 @@
 package model; //Na aula o package é emailapp
 
+import java.util.Scanner;
+
 public class Email {
     
     private String firstName;
@@ -14,11 +16,25 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("Email created: " + this.firstName + " " + this.lastName);
+
+        // Call a method for the department - return the departament
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
     }
 
-
     // Ask for the department
-
+    private String setDepartment() {
+        System.out.print("Department Codes:\n 1 for Sale\n 2 for Development\n 3 for Accounting\n 0 for none\n Enter your department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        // Loop could been done here
+        if (depChoice == 1) { return "sales"; }
+        else if (depChoice == 2) { return "dev"; }
+        else if (depChoice == 3) { return "acct"; }
+        // else if (depChoice == 0) { return "back to menu"; }
+        // else { return "error, please select a valid number"; }
+        else { return ""; }
+    }
     
     // Generate a radom password
 
